218e1063ebf4fa788a47cee418665e7e00a90e80
Mostafa-Usama <mostafa.usama286@gmail.com>
2023-03-07 18:28:10 +0200

    Added logo

--- Face detection/Interface.py
+++ Face detection/Interface.py
@@ -15,12 +15,13 @@ def draw():
     
     
     root = Tk()
+    # root.configure(bg="black")
     style = tk.Style()
-    style.configure("BW.TRadiobutton",font=("Arial",17))
+    style.configure("TRadiobutton",font=("Arial",17),background="#F0F0F0")
     root.title("Smart Classroom")
-    root.geometry("800x600")
+    root.geometry("800x650")
     root.resizable(False,False)
-    root.iconbitmap("logo2.ico")
+    root.iconbitmap("icons/logo2.ico")
     
     r = IntVar()
     
@@ -56,7 +57,7 @@ def draw():
         
 
     r.set("1")
-    
+
     def mode(value):
         global auto 
         if value == 1:
@@ -87,25 +88,24 @@ def draw():
     
 
 
-    plus_image = PhotoImage(file="plus.png")
-    minus_image = PhotoImage(file="minus.png")
-    on_image = PhotoImage(file="on.png")
-    off_image = PhotoImage(file="off.png")
-    logo_image = PhotoImage(file="logo_image.png")
+    plus_image = PhotoImage(file="icons/plus.png")
+    minus_image = PhotoImage(file="icons/minus.png")
+    on_image = PhotoImage(file="icons/on.png")
+    off_image = PhotoImage(file="icons/off.png")
+    logo_image = PhotoImage(file="icons/SCR-logo2.png")
     
     l = Label(image=logo_image)   
-    configs =Label(root,text="Configurations",fg="#00BDC9",font=("Arial",30,"italic","underline"),padx=5)
-    line = Label(root,fg="#134982",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
+    configs =Label(root,text="Configurations",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
+    line = Label(root,fg="black",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
     number = Label(root,text=f"Students per section", font=("Arial",20,"bold"),fg="#134982")
     students = Label(root,text=MAX_NUMBER,font=("Arial",20),fg="black")
     increase = Button(root,text="+",font=("Arial",15),command=lambda:adjustNumber(1),bd=0,image=plus_image)
     decrease = Button(root,text="-",font=("Arial",15),padx=3,command=lambda:adjustNumber(2),image=minus_image,bd=0)
-
     modes = Label(root,text="Modes",font=("Arial",20,"bold"),fg="#134982")
-    autos = tk.Radiobutton(root,text="Auto",variable=r,value=1,command=lambda:mode(r.get()),style="BW.TRadiobutton")
-    manual = tk.Radiobutton(root,text="Manual",variable=r,value=2,command=lambda:mode(r.get()),style="BW.TRadiobutton")
+    autos = tk.Radiobutton(root,text="Auto",variable=r,value=1,command=lambda:mode(r.get()))
+    manual = tk.Radiobutton(root,text="Manual",variable=r,value=2,command=lambda:mode(r.get()))
 
-    devices =Label(root,text="Device Control",fg="#00BDC9",font=("Arial",30,"italic","underline"),padx=5)
+    devices =Label(root,text="Device Control",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
     
 
     green = Label(root,text="Green LED", font=("Arial",20,"bold"),fg="#134982")
@@ -114,27 +114,28 @@ def draw():
     ton2 = Button(root,image=on_image,command=lambda:on(7),bd=0)
 
 
-    l.place(x=225,y=-80)
+    l.place(x=280,y=-35)
     
-    configs.place(x=260,y=130)
-    
-    number.place(x=30,y=220)
-    increase.place(x=200,y=270)
-    students.place(x=145,y=270)
-    decrease.place(x=80,y=270)
 
-    modes.place(x=600,y=220)
-    autos.place(x=550,y=270)
-    manual.place(x=650,y=270)
+    configs.place(x=270,y=180)
     
-    line.place(x=0,y=300)
+    number.place(x=30,y=270)
+    increase.place(x=200,y=320)
+    students.place(x=145,y=320)
+    decrease.place(x=80,y=320)
+
+    modes.place(x=600,y=270)
+    autos.place(x=550,y=320)
+    manual.place(x=650,y=320)
     
-    devices.place(x=260,y=380)
+    line.place(x=0,y=350)
     
-    green.place(x=560,y=450)
-    ton1.place(x=600,y=500)
-    red.place(x=60,y=450)
-    ton2.place(x=80,y=500)
+    devices.place(x=260,y=400)
+    
+    green.place(x=560,y=500)
+    ton1.place(x=600,y=550)
+    red.place(x=60,y=500)
+    ton2.place(x=80,y=550)
 
     mode(1)
     mainloop()
@@ -147,4 +148,4 @@ def getMax():
     return MAX_NUMBER
 
 
-#draw()
+draw()
