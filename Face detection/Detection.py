132b69bd95cd15a2763ef5d03c733b96c1fa1edc
Mostafa-Usama <mostafa.usama286@gmail.com>
2023-03-14 00:56:56 +0200

    login,admin and user windows

--- Face detection/Detection.py
+++ Face detection/Detection.py
@@ -1,6 +1,6 @@
 import cv2
 from tkinter import * 
-from Interface import draw,s,getMode,getMax
+from Interface import s,getMode,getMax, Application_Interface
 import _thread
 from Graph_Head import FROZEN_GRAPH_HEAD
 import time
@@ -8,7 +8,8 @@ import time
 left = 0
 right = 0
 Max_Number = getMax()
-thread = _thread.start_new_thread(draw,())
+app = Application_Interface()
+thread = _thread.start_new_thread(app.run,())
 
 
 PATH_TO_CKPT_HEAD = 'models/HEAD_DETECTION_300x300_ssd_mobilenetv2.pb'
@@ -20,56 +21,61 @@ source = 'test.mp4'
 tDetector = FROZEN_GRAPH_HEAD(PATH_TO_CKPT_HEAD)
 #cap = cv2.VideoCapture(source)
 
-while True:
+while app.opened:
     
-    auto = getMode()   
-    while auto:
-        Max_Number = getMax()
-        auto = getMode()  
-        image = cv2.imread("Test/7.jpg")
-        #ret, image = cap.read()
-        # if ret == 0:
-        #     break
+    if app.logged:
+        auto = getMode()   
+        while auto and app.opened:
+            Max_Number = getMax()
+            auto = getMode()  
+            image = cv2.imread("Test/7.jpg")
+            #ret, image = cap.read()
+            # if ret == 0:
+            #     break
 
-        im_height, im_width, im_channel = image.shape
-        image = cv2.flip(image, 1)
+            im_height, im_width, im_channel = image.shape
+            image = cv2.flip(image, 1)
 
-        boxes, scores, right, left = tDetector.run(image,im_width,im_height)
-        cv2.imshow("HEAD DETECTION USING FROZEN GRAPH", image)
+            boxes, scores, right, left = tDetector.run(image,im_width,im_height)
+            cv2.imshow("HEAD DETECTION USING FROZEN GRAPH", image)
 
-        k = cv2.waitKey(1) & 0xff
-        if k == ord('q') or k == 27:
-            break
+            k = cv2.waitKey(1) & 0xff
+            if k == ord('q') or k == 27:
+                break
 
-        # cv2.line(image, (im_width//2, 0), (im_width//2, im_height), (0, 255, 0), 2)
+            # cv2.line(image, (im_width//2, 0), (im_width//2, im_height), (0, 255, 0), 2)
 
-            
-
-                #cv2.putText(img,"Count of faces:"+str(len(faces)),(width//3,heghit//6),font,.9,(0,0,255),3) 
-                #text (image, string, bottom left point, font, font size, color, thickness)
-            #cv2.imshow('img',img)    # show image 
-
-        print(f'Left: {left}', f"right: {right}")
-
-        if left >= Max_Number and right >= Max_Number:
-            fe = '1'
-            s.write(fe.encode('utf-8'))
-        elif left >= Max_Number and right < Max_Number:
-            fe = '2'
-            s.write(fe.encode('utf-8'))
-        elif left < Max_Number and right >= Max_Number:
-            fe = '3'
-            s.write(fe.encode('utf-8'))
-        else:
-            fe = '4'
-            s.write(fe.encode('utf-8'))
-        print(fe)
-        time.sleep(10)
-        #cap.release()
-
-        #cv2.destroyAllWindows()
                 
-    #cv2.waitKey(0) # wait for button or X 
-    #cv2.destroyAllWindows()
+
+                    #cv2.putText(img,"Count of faces:"+str(len(faces)),(width//3,heghit//6),font,.9,(0,0,255),3) 
+                    #text (image, string, bottom left point, font, font size, color, thickness)
+                #cv2.imshow('img',img)    # show image 
+
+            print(f'Left: {left}', f"right: {right}")
+
+            if left >= Max_Number and right >= Max_Number:
+                fe = '1'
+                s.write(fe.encode('utf-8'))
+            elif left >= Max_Number and right < Max_Number:
+                fe = '2'
+                s.write(fe.encode('utf-8'))
+            elif left < Max_Number and right >= Max_Number:
+                fe = '3'
+                s.write(fe.encode('utf-8'))
+            else:
+                fe = '4'
+                s.write(fe.encode('utf-8'))
+            print(fe)
+
+            for i in range(100):
+                if not app.opened:
+                    break
+                time.sleep(0.1)
+            #cap.release()
+
+            #cv2.destroyAllWindows()
+                    
+        #cv2.waitKey(0) # wait for button or X 
+        #cv2.destroyAllWindows()
 
 
--- Face detection/Interface.py
+++ Face detection/Interface.py
@@ -1,151 +1,370 @@
 import serial 
 from tkinter import * 
 import tkinter.ttk as tk
+import time
 
-
+s = serial.Serial('COM3',9600)
 global MAX_NUMBER
 MAX_NUMBER = 10
 global auto
 auto = True
-flag_green = False
-flag_red = False
-s = serial.Serial('COM3',9600)
 
-def draw():
+class Application_Interface:
+    logged=False
+    opened=True
+    def run(self):
+        self.window = Tk()
+
+        def disable_event():
+            pass
+        
+        self.window.protocol("WM_DELETE_WINDOW", disable_event)
+        self.window.resizable(False,False)
+        self.window.geometry("800x650+250+50")
+        self.window.iconbitmap("icons\lamp.ico")
+        self.window.title("Login")
+        txt ="Welcome, you'll see that saving power will be easier than before!" 
+        self.welcome = Label(self.window,font=("Arial",15,"italic"),fg="Green")
+        self.quit = Button(self.window,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
+        self.logo=PhotoImage(file="icons\SCR-logo2.png")
+        self.usernameEntry = Entry(self.window, width=22,font=("bold",15))  
+        self.passwordEntry = Entry(self.window, width=22,font=("bold",15),show="*")
+        self.usernameLabel = Label(self.window, text="Username",font=('Sitka Small', 15, 'bold'))
+        self.passwordLabel = Label(self.window, text="Password",font=('Sitka Small', 15, 'bold'))
+        self.userlogin=Button(self.window,text="Login as User",command=self.check_user,padx=20, pady=5, fg="white",bg="#B33030", font=('calibre', 10, 'bold'),activebackground="#B33030",activeforeground="white")
+        self.message_label =  Label(self.window, text="",font=("Arial",10,"bold"),fg="red")
+        self.logo_image = PhotoImage(file="icons/SCR-logo2.png")
+        self.logo = Label(image=self.logo_image)
+        self.adminlogin=Button(self.window,text="Login as Admin",padx=20, pady=5, fg="white",bg="#B33030", font=('calibre', 10, 'bold'),command=self.check_admin,activebackground="#B33030",activeforeground="white")
+
+        self.quit.place(x=78,y=0)
+        self.userlogin.place(x=510, y=580)
+        self.adminlogin.place(x= 170, y=580)
+        self.logo.place(x=290,y=-20)
+        self.message_label.place(x=280,y=520)
+        self.usernameEntry.place(x=370,y=335,height=28)
+        self.passwordEntry.place(x=370,y=435,height=28)  
+        self.usernameLabel.place(x=200,y=330)
+        self.passwordLabel.place(x=200,y=430)
+
+        self.printString(txt)
+        self.window.mainloop()
+
+    def des(self):
+
+            self.opened=False
+
+
+    def printString(self, string):
+        for char in string:
+            self.welcome.configure(text=self.welcome.cget('text') + char)
+            self.welcome.update()
+            self.welcome.place(x=100,y=230)
+            time.sleep(.07)
+
+    def register(self):
+        username = self.usernameEntry.get()
+        password = self.passwordEntry.get()
+
+        if username and password:
+            try:
+                self.cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+                self.conn.commit()
+                self.message_label.config(text="Registration successful")
+            except:
+                self.message_label.config(text="Username already exists")
+        else:
+            self.message_label.config(text="Please enter a valid username and password")
+
+    def login(self):
+        username = self.usernameEntry.get()
+        password = self.passwordEntry.get()
+
+        if username and password:
+            self.cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+            user = self.cursor.fetchone()
+
+            if user:
+                self.window.withdraw()
+                self.draw()
+
+            else:
+                self.message_label.config(text="Please enter a valid username and password")
+        else:
+            self.message_label.config(text="Please enter a valid username and password")
     
+    def check_user(self):
+        username = self.usernameEntry.get()
+        password = self.passwordEntry.get()
+
+        if username=="fci" and password=="1234":
+            self.window.withdraw()
+            self.logged=True
+            self.draw()      
+        else:
+            self.message_label.config(text="Please enter a valid username and password")
+
+
+    def check_admin(self):
+        username = self.usernameEntry.get()
+        password = self.passwordEntry.get()
+
+        if username=="admin" and password=="admin":
+            self.window.withdraw()
+            self.logged=True
+            self.Admin_Page()      
+        else:
+            self.message_label.config(text="Please enter a valid username and password")
+
+    def Admin_Page(self):
     
-    root = Tk()
-    # root.configure(bg="black")
-    style = tk.Style()
-    style.configure("TRadiobutton",font=("Arial",17),background="#F0F0F0")
-    root.title("Smart Classroom")
-    root.geometry("800x650")
-    root.resizable(False,False)
-    root.iconbitmap("icons/logo2.ico")
-    
-    r = IntVar()
-    
-    def on(num):
+        root = Toplevel()
+
+        def disable_event():
+            pass
+        
+        root.protocol("WM_DELETE_WINDOW", disable_event)
+        root.title("Smart Classroom")
+        root.geometry("800x650+250+50")
+        root.resizable(False,False)
+        root.iconbitmap("icons\lamp.ico")
+        
+        quit = Button(root,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')        
+        logo_image = PhotoImage(file="icons\SCR-logo2.png")
+        l = Label(root,image=logo_image)   
+        insert=Label(root,text="Insert User",font=('Sitka Small', 15, 'bold'))
+        user=Label(root,text="Username",font=("Arial",15))
+        password=Label(root,text="Password",font=("Arial",15))
+        usere=Entry(root,width=40)
+        passe=Entry(root,width=40)
+        line = Label(root,fg="black",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
+        Delete_user=Label(root,text="Delete User",font=('Sitka Small', 15, 'bold'))
+        delete=Entry(root,width=40)
+        user_delete_label=Label(root,text="Username",font=("Arial",15))
+        insert_button=Button(root,text="Insert",width=20, padx=7, pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
+        Delete_button=Button(root,text="Delete",width=20, padx=7, pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
+
+        quit.place(x=78,y=0)
+        l.place(x=280,y=-35)
+        insert.place(x=330,y=200)
+        user.place(x=200,y=270)
+        password.place(x=200,y=350)
+        usere.place(x=330,y=280)
+        passe.place(x=330,y=360)
+        line.place(x=0,y=420)
+        Delete_user.place(x=330,y=480)
+        user_delete_label.place(x=200,y=530)
+        delete.place(x=330,y=540)
+        insert_button.place(x=550,y=400)
+        Delete_button.place(x=550,y=580)
+
+        mainloop()
+
+
+    def draw(self):
+        
         global flag_green
         global flag_red
-        global MAX_NUMBER
-        if num == 5:
-          
-            if flag_green:
-                ton1.config(image=on_image)
-                flag_green = False
-                fe = '5'
-                s.write(fe.encode('utf-8'))
-            else:
-                ton1.config(image=off_image)
-                flag_green = True
-                fe = '6'
-                s.write(fe.encode('utf-8'))
+        global darkMood
+        darkMood = False
+        flag_green = False
+        flag_red = False
+        style = tk.Style()
+        root = Toplevel()
 
-       
-        elif num == 7:
-            if flag_red:
-                ton2.config(image=on_image)
-                flag_red = False
-                fe = '7'
-                s.write(fe.encode('utf-8'))
-            else:
-                ton2.config(image=off_image)
-                flag_red = True
-                fe = '8'
-                s.write(fe.encode('utf-8'))
+        def disable_event():
+            pass
         
+        root.protocol("WM_DELETE_WINDOW", disable_event)
 
-    r.set("1")
+        style.configure("BW.TRadiobutton",font=("Arial",17))
+        root.title("Smart Classroom")
+        root.geometry("800x650+250+50")
+        root.resizable(False,False)
+        root.iconbitmap("icons/lamp.ico")    
+        r = IntVar()
+        
+        def on(num):
+            global flag_green
+            global flag_red
+            global MAX_NUMBER
+            global auto
+            
+            if auto:
+                ton1.config(command=None)
+                ton2.config(command=None)
+                # show error message 
+                
+            elif num == 5:
+            
+                if flag_green:
+                    ton1.config(image=on_image)
+                    flag_green = False
+                    fe = '5'
+                    s.write(fe.encode('utf-8'))
+                else:
+                    ton1.config(image=off_image)
+                    flag_green = True
+                    fe = '6'
+                    s.write(fe.encode('utf-8'))
 
-    def mode(value):
-        global auto 
-        if value == 1:
-            auto = True
-            getMode()
-            print(auto)
-            ton1.config(state=DISABLED)
-            ton2.config(state=DISABLED)
-    
-        elif value == 2:  
-            auto = False
-            getMode()
-            print(auto)    
-            ton1.config(state=ACTIVE)
-            ton2.config(state=ACTIVE)
+        
+            elif num == 7:
+               
+                if flag_red:
+                    ton2.config(image=on_image)
+                    flag_red = False
+                    fe = '7'
+                    s.write(fe.encode('utf-8'))
+                else:
+                    ton2.config(image=off_image)
+                    flag_red = True
+                    fe = '8'
+                    s.write(fe.encode('utf-8'))
             
 
+        r.set("1")
 
-    def adjustNumber(value):
-        global MAX_NUMBER
-        if value == 1:
-            MAX_NUMBER += 1
-            students.config(text=MAX_NUMBER)
-        else:
-            if not (MAX_NUMBER <= 1):
-                MAX_NUMBER -= 1
+        def dark():
+            global darkMood
+
+            if not darkMood:
+                darkMood = True
+                dark_mood.config(text="Light Mood",bg='black',fg='#F0F0F0')                
+                root.configure(bg="#111111")
+                style.configure("TRadiobutton",font=("Arial",17),background="#111111",foreground="white")
+                l.config(bg="#111111")
+                configs.config(bg="#111111",fg="#4E31AA")
+                line.config(bg="#111111",fg="white")
+                number.config(bg="#111111",fg="#E94560")
+                students.config(bg="#111111",fg="white")
+                increase.config(bg="#111111",activebackground="#111111")
+                decrease.config(bg="#111111",activebackground="#111111")
+                modes.config(bg="#111111",fg="#E94560")
+                #autos.config(bg="#111111")
+                #manual.config(bg="#111111")
+                devices.config(bg="#111111",fg="#4E31AA")
+                green.config(bg="#111111",fg="#E94560")
+                red.config(bg="#111111",fg="#E94560")
+                ton1.config(bg="#111111",activebackground="#111111")
+                ton2.config(bg="#111111",activebackground="#111111")
+            else:
+                darkMood = False
+                dark_mood.config(text="Dark Mood",bg='black',fg='#F0F0F0')
+                root.configure(bg="#F0F0F0")
+                style.configure("TRadiobutton",font=("Arial",17),background="#F0F0F0",foreground="black")
+                l.config(bg="#F0F0F0")
+                configs.config(bg="#F0F0F0",fg="#AA0121")
+                line.config(bg="#F0F0F0",fg="black")
+                number.config(bg="#F0F0F0",fg="#134982")
+                students.config(bg="#F0F0F0",fg="black")
+                increase.config(bg="#F0F0F0",activebackground="#F0F0F0")
+                decrease.config(bg="#F0F0F0",activebackground="#F0F0F0")
+                modes.config(bg="#F0F0F0",fg="#134982")
+                #autos.config(bg="#111111")
+                #manual.config(bg="#111111")
+                devices.config(bg="#F0F0F0",fg="#AA0121")
+                green.config(bg="#F0F0F0",fg="#134982")
+                red.config(bg="#F0F0F0",fg="#134982")
+                ton1.config(bg="#F0F0F0",activebackground="#F0F0F0")
+                ton2.config(bg="#F0F0F0",activebackground="#F0F0F0")
+
+            
+        def mode(value):
+            global auto 
+            if value == 1:
+                auto = True
+                getMode()
+                print(auto)
+                        
+            elif value == 2:  
+                auto = False
+                getMode()
+                print(auto)    
+                ton1.config(state=ACTIVE,command=lambda:on(5))
+                ton2.config(state=ACTIVE,command=lambda:on(7))
+                
+
+
+        def adjustNumber(value):
+            global MAX_NUMBER
+            if value == 1:
+                MAX_NUMBER += 1
                 students.config(text=MAX_NUMBER)
+            else:
+                if not (MAX_NUMBER <= 1):
+                    MAX_NUMBER -= 1
+                    students.config(text=MAX_NUMBER)
+        
+        def des():
+            self.opened=False
+            #self.window.destroy()
+
+        plus_image = PhotoImage(file="icons/plus.png")
+        minus_image = PhotoImage(file="icons/minus.png")
+        on_image = PhotoImage(file="icons/on.png")
+        off_image = PhotoImage(file="icons/off.png")
+        logo_image = PhotoImage(file="icons/SCR-logo2.png")
+        
+        l = Label(root,image=logo_image)   
+        configs =Label(root,text="Configurations",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
+        line = Label(root,fg="#134982",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
+        number = Label(root,text=f"Students per section", font=("Arial",20,"bold"),fg="#134982")
+        students = Label(root,text=MAX_NUMBER,font=("Arial",20),fg="black")
+        increase = Button(root,text="+",font=("Arial",15),command=lambda:adjustNumber(1),bd=0,image=plus_image)
+        decrease = Button(root,text="-",font=("Arial",15),padx=3,command=lambda:adjustNumber(2),image=minus_image,bd=0)
+        
+        dark_mood = Button(root,text="Dark Mood",command=dark,width=8,font=("Arial 10 bold") ,bd=3,bg="black",fg='White',activeforeground='white',activebackground='black')
+        quit = Button(root,text="Exit",command=des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
+        modes = Label(root,text="Modes",font=("Arial",20,"bold"),fg="#134982")
+        autos = tk.Radiobutton(root,text="Auto",variable=r,value=1,command=lambda:mode(r.get()),style="BW.TRadiobutton")
+        manual = tk.Radiobutton(root,text="Manual",variable=r,value=2,command=lambda:mode(r.get()),style="BW.TRadiobutton")
+
+        devices =Label(root,text="Device Control",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
+        
+
+        green = Label(root,text="Green LED", font=("Arial",20,"bold"),fg="#134982")
+        red = Label(root,text="Red LED",font=("Arial",20,"bold"),fg="#134982")
+        ton1 = Button(root,image=on_image,command=lambda:on(5),bd=0,padx=20)
+        ton2 = Button(root,image=on_image,command=lambda:on(7),bd=0)
+
+
+        l.place(x=280,y=-35)
+        
+        dark_mood.place(x=0,y=0)
+        quit.place(x=78,y=0)
+
+        configs.place(x=270,y=180)
+        
+        number.place(x=30,y=270)
+        increase.place(x=200,y=320)
+        students.place(x=145,y=320)
+        decrease.place(x=80,y=320)
+
+        modes.place(x=600,y=270)
+        autos.place(x=550,y=320)
+        manual.place(x=650,y=320)
+        
+        line.place(x=0,y=350)
+        
+        devices.place(x=260,y=400)
+        
+        green.place(x=560,y=500)
+        ton1.place(x=600,y=550)
+        red.place(x=60,y=500)
+        ton2.place(x=80,y=550)
+
+
+        mode(1)
+        mainloop()
+
     
-
-
-    plus_image = PhotoImage(file="icons/plus.png")
-    minus_image = PhotoImage(file="icons/minus.png")
-    on_image = PhotoImage(file="icons/on.png")
-    off_image = PhotoImage(file="icons/off.png")
-    logo_image = PhotoImage(file="icons/SCR-logo2.png")
-    
-    l = Label(image=logo_image)   
-    configs =Label(root,text="Configurations",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
-    line = Label(root,fg="black",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
-    number = Label(root,text=f"Students per section", font=("Arial",20,"bold"),fg="#134982")
-    students = Label(root,text=MAX_NUMBER,font=("Arial",20),fg="black")
-    increase = Button(root,text="+",font=("Arial",15),command=lambda:adjustNumber(1),bd=0,image=plus_image)
-    decrease = Button(root,text="-",font=("Arial",15),padx=3,command=lambda:adjustNumber(2),image=minus_image,bd=0)
-    modes = Label(root,text="Modes",font=("Arial",20,"bold"),fg="#134982")
-    autos = tk.Radiobutton(root,text="Auto",variable=r,value=1,command=lambda:mode(r.get()))
-    manual = tk.Radiobutton(root,text="Manual",variable=r,value=2,command=lambda:mode(r.get()))
-
-    devices =Label(root,text="Device Control",fg="#AA0121",font=("Arial",30,"italic","underline"),padx=5)
-    
-
-    green = Label(root,text="Green LED", font=("Arial",20,"bold"),fg="#134982")
-    red = Label(root,text="Red LED",font=("Arial",20,"bold"),fg="#134982")
-    ton1 = Button(image=on_image,command=lambda:on(5),bd=0,padx=20)
-    ton2 = Button(root,image=on_image,command=lambda:on(7),bd=0)
-
-
-    l.place(x=280,y=-35)
-    
-
-    configs.place(x=270,y=180)
-    
-    number.place(x=30,y=270)
-    increase.place(x=200,y=320)
-    students.place(x=145,y=320)
-    decrease.place(x=80,y=320)
-
-    modes.place(x=600,y=270)
-    autos.place(x=550,y=320)
-    manual.place(x=650,y=320)
-    
-    line.place(x=0,y=350)
-    
-    devices.place(x=260,y=400)
-    
-    green.place(x=560,y=500)
-    ton1.place(x=600,y=550)
-    red.place(x=60,y=500)
-    ton2.place(x=80,y=550)
-
-    mode(1)
-    mainloop()
-
 
 
 def getMode():
     return auto
 def getMax():
     return MAX_NUMBER
-
-
-draw()
+    
+app = Application_Interface()
+app.run()
