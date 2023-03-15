04ff3e9a2621d76cabc98ecfbbaae709120091ac
Mohamed-Nabi1 <Mohamedd.Nabil3@gmail.com>
2023-03-15 18:05:07 +0200

    added database

--- Face detection/Interface.py
+++ Face detection/Interface.py
@@ -1,7 +1,9 @@
 import serial 
 from tkinter import * 
+from tkinter import messagebox
 import tkinter.ttk as tk
 import time
+import sqlite3
 
 s = serial.Serial('COM3',9600)
 global MAX_NUMBER
@@ -16,28 +18,28 @@ class Application_Interface:
         self.window = Tk()
 
         def disable_event():
-            pass
+            self.opened = False
         
         self.window.protocol("WM_DELETE_WINDOW", disable_event)
         self.window.resizable(False,False)
         self.window.geometry("800x650+250+50")
-        self.window.iconbitmap("icons\lamp.ico")
+        # self.window.iconbitmap("icons\lamp.ico")
         self.window.title("Login")
         txt ="Welcome, you'll see that saving power will be easier than before!" 
         self.welcome = Label(self.window,font=("Arial",15,"italic"),fg="Green")
-        self.quit = Button(self.window,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
+       # self.quit = Button(self.window,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
         self.logo=PhotoImage(file="icons\SCR-logo2.png")
         self.usernameEntry = Entry(self.window, width=22,font=("bold",15))  
         self.passwordEntry = Entry(self.window, width=22,font=("bold",15),show="*")
         self.usernameLabel = Label(self.window, text="Username",font=('Sitka Small', 15, 'bold'))
         self.passwordLabel = Label(self.window, text="Password",font=('Sitka Small', 15, 'bold'))
-        self.userlogin=Button(self.window,text="Login as User",command=self.check_user,padx=20, pady=5, fg="white",bg="#B33030", font=('calibre', 10, 'bold'),activebackground="#B33030",activeforeground="white")
+        self.userlogin=Button(self.window,text="Login as User",command=self.login,padx=20, pady=5, fg="white",bg="#B33030", font=('calibre', 10, 'bold'),activebackground="#B33030",activeforeground="white")
         self.message_label =  Label(self.window, text="",font=("Arial",10,"bold"),fg="red")
         self.logo_image = PhotoImage(file="icons/SCR-logo2.png")
         self.logo = Label(image=self.logo_image)
         self.adminlogin=Button(self.window,text="Login as Admin",padx=20, pady=5, fg="white",bg="#B33030", font=('calibre', 10, 'bold'),command=self.check_admin,activebackground="#B33030",activeforeground="white")
 
-        self.quit.place(x=78,y=0)
+        #self.quit.place(x=78,y=0)
         self.userlogin.place(x=510, y=580)
         self.adminlogin.place(x= 170, y=580)
         self.logo.place(x=290,y=-20)
@@ -46,7 +48,10 @@ class Application_Interface:
         self.passwordEntry.place(x=370,y=435,height=28)  
         self.usernameLabel.place(x=200,y=330)
         self.passwordLabel.place(x=200,y=430)
-
+        self.con = sqlite3.connect("Database.db")
+        self.cr = self.con.cursor()
+        self.cr.execute(""" CREATE TABLE IF NOT EXISTS users (ID INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(30), password VARCHAR(30)) """)
+        self.con.commit() 
         self.printString(txt)
         self.window.mainloop()
 
@@ -81,28 +86,29 @@ class Application_Interface:
         password = self.passwordEntry.get()
 
         if username and password:
-            self.cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
-            user = self.cursor.fetchone()
+            self.cr.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+            user = self.cr.fetchone()
 
             if user:
                 self.window.withdraw()
+                self.logged = True
                 self.draw()
 
             else:
-                self.message_label.config(text="Please enter a valid username and password")
+                messagebox.showerror("Failed","Please enter a valid username and password")
         else:
-            self.message_label.config(text="Please enter a valid username and password")
+            messagebox.showerror("Failed","Please enter a valid username and password")
     
-    def check_user(self):
-        username = self.usernameEntry.get()
-        password = self.passwordEntry.get()
+    # def check_user(self):
+    #     username = self.usernameEntry.get()
+    #     password = self.passwordEntry.get()
 
-        if username=="fci" and password=="1234":
-            self.window.withdraw()
-            self.logged=True
-            self.draw()      
-        else:
-            self.message_label.config(text="Please enter a valid username and password")
+    #     if username=="fci" and password=="1234":
+    #         self.window.withdraw()
+    #         self.logged=True
+    #         self.draw()      
+    #     else:
+    #         self.message_label.config(text="Please enter a valid username and password")
 
 
     def check_admin(self):
@@ -113,14 +119,54 @@ class Application_Interface:
             self.window.withdraw()
             self.Admin_Page()      
         else:
-            self.message_label.config(text="Please enter a valid username and password")
+            messagebox.showerror("Failed","Please enter a valid username and password")
 
     def Admin_Page(self):
     
         root = Toplevel()
 
         def disable_event():
-            pass
+            self.opened = False
+        
+        def insert_user():
+            username = usere.get()
+            passw = passe.get()
+            if username and password:
+                self.cr.execute("SELECT * FROM users WHERE username=?", (username,))
+                user = self.cr.fetchone()
+                if not user:
+                    con = sqlite3.connect("Database.db")
+                    cur = con.cursor()
+                    cur.execute("INSERT INTO users (username,password) VALUES (?,?)",(username,passw))
+                    con.commit()
+                    message_label.config(text="User inserted succesfully!",fg="green")
+                    message_label.place(x=360,y=400)
+
+                else:
+                    messagebox.showerror("Failed","User already exists!")
+                    
+            else:
+                messagebox.showerror("Failed","Please enter a valid username and password")
+                    
+        def delete_user():
+            username = delete.get()
+            if username:
+                self.cr.execute("SELECT * FROM users WHERE username=?", (username,))
+                user = self.cr.fetchone()
+                if user:
+                    con = sqlite3.connect("Database.db")
+                    cur = con.cursor()
+                    cur.execute("DELETE FROM users WHERE username=?",(username,))
+                    con.commit()
+                    message_label.config(text="User deleted succesfully!",fg="green")
+                    message_label.place(x=360,y=580)
+                else:
+                    messagebox.showerror("Failed","User doesn't exist!")
+
+
+            else:
+                messagebox.showerror("Failed","Please enter a valid username and password")
+
         
         root.protocol("WM_DELETE_WINDOW", disable_event)
         root.title("Smart Classroom")
@@ -128,34 +174,37 @@ class Application_Interface:
         root.resizable(False,False)
         root.iconbitmap("icons\lamp.ico")
         
-        quit = Button(root,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')        
+        #quit = Button(root,text="Exit",command=self.des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')        
         logo_image = PhotoImage(file="icons\SCR-logo2.png")
+        message_label =  Label(root, text="",font=("Arial",10,"bold"),fg="red")
         l = Label(root,image=logo_image)   
         insert=Label(root,text="Insert User",font=('Sitka Small', 15, 'bold'))
         user=Label(root,text="Username",font=("Arial",15))
         password=Label(root,text="Password",font=("Arial",15))
-        usere=Entry(root,width=40)
-        passe=Entry(root,width=40)
+        usere= Entry(root, width=22,font=("bold",15))
+        passe= Entry(root, width=22,font=("bold",15),show="*")
         line = Label(root,fg="black",text="___________________________________________________________________________________________________________________________________________________________________",font=("Arial",20))
         Delete_user=Label(root,text="Delete User",font=('Sitka Small', 15, 'bold'))
-        delete=Entry(root,width=40)
+        delete= Entry(root, width=22,font=("bold",15))
         user_delete_label=Label(root,text="Username",font=("Arial",15))
-        insert_button=Button(root,text="Insert",width=20, padx=7, pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
-        Delete_button=Button(root,text="Delete",width=20, padx=7, pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
+        insert_button=Button(root,text="Insert",width=20, padx=7, command=insert_user,pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
+        Delete_button=Button(root,text="Delete",width=20, padx=7,command=delete_user, pady=5, bg="#B33030", fg="white", font=('calibre', 10, 'bold'))
 
-        quit.place(x=78,y=0)
+        #quit.place(x=78,y=0)
+        #message_label.place(x=360,y=380)
         l.place(x=280,y=-35)
         insert.place(x=330,y=200)
         user.place(x=200,y=270)
         password.place(x=200,y=350)
-        usere.place(x=330,y=280)
-        passe.place(x=330,y=360)
+        usere.place(x=330,y=280,height=28)
+        passe.place(x=330,y=360,height=28)
         line.place(x=0,y=420)
-        Delete_user.place(x=330,y=480)
+        Delete_user.place(x=330,y=480,height=28)
         user_delete_label.place(x=200,y=530)
         delete.place(x=330,y=540)
         insert_button.place(x=550,y=400)
         Delete_button.place(x=550,y=580)
+        
 
         mainloop()
 
@@ -172,7 +221,7 @@ class Application_Interface:
         root = Toplevel()
 
         def disable_event():
-            pass
+            self.opened = False
         
         root.protocol("WM_DELETE_WINDOW", disable_event)
 
@@ -314,7 +363,7 @@ class Application_Interface:
         decrease = Button(root,text="-",font=("Arial",15),padx=3,command=lambda:adjustNumber(2),image=minus_image,bd=0)
         
         dark_mood = Button(root,text="Dark Mood",command=dark,width=8,font=("Arial 10 bold") ,bd=3,bg="black",fg='White',activeforeground='white',activebackground='black')
-        quit = Button(root,text="Exit",command=des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
+        #quit = Button(root,text="Exit",command=des,width=8,font=("Arial 10 bold") ,bd=3,bg="#AA0121",fg='white',activebackground='#AA0121')
         modes = Label(root,text="Modes",font=("Arial",20,"bold"),fg="#134982")
         autos = tk.Radiobutton(root,text="Auto",variable=r,value=1,command=lambda:mode(r.get()),style="BW.TRadiobutton")
         manual = tk.Radiobutton(root,text="Manual",variable=r,value=2,command=lambda:mode(r.get()),style="BW.TRadiobutton")
@@ -331,7 +380,7 @@ class Application_Interface:
         l.place(x=280,y=-35)
         
         dark_mood.place(x=0,y=0)
-        quit.place(x=78,y=0)
+        #quit.place(x=78,y=0)
 
         configs.place(x=270,y=180)
         
@@ -366,4 +415,4 @@ def getMax():
     return MAX_NUMBER
     
 app = Application_Interface()
-#app.run()
+app.run()
