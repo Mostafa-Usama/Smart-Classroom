42ce536b41e8ac772f808f81cfe33acb5b4b32a2
Mohamed-Nabi1 <Mohamedd.Nabil3@gmail.com>
2023-07-03 22:23:58 +0300

    LAST Version

--- head detection/Roboflow.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="GN6rbgSIiaObyAPShVga")
-project = rf.workspace("assiut-university").project("head-detection-t4zes")
-dataset = project.version(2).download("yolov8")
